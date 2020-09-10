4690969fad0ea32d1c8bedda4ec94c1d75e1648e
Susanna Aro <susanna@vainu.io>
2020-09-10 08:08:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,32 +10,31 @@ from setuptools import setup, find_packages
 if sys.version_info[0] < 3:
     if sys.version_info[1] < 7:
         raise ValueError("Only python >= 2.7 is supported")
-    INSTALL_REQUIRES = ['pylint==1.9', 'mock']
+    INSTALL_REQUIRES = ["pylint==1.9", "mock"]
 else:
-    INSTALL_REQUIRES = ['pylint>2.0']
+    INSTALL_REQUIRES = ["pylint>2.0"]
 
-LONG_DESCRIPTION = open('README.md').read()
+LONG_DESCRIPTION = open("README.md").read()
 
 setup(
-    name='vainupylinter',
-    url='https://github.com/vainuio/vainupylinter',
-    author='susanna@vainu.io',
-    author_email='susanna@vainu.io',
-    description='A custom pylint runner for pylint in use in some Vainu repositories',
-    version='1.0',
+    name="vainupylinter",
+    url="https://github.com/vainuio/vainupylinter",
+    author="susanna@vainu.io",
+    author_email="susanna@vainu.io",
+    description="A custom pylint runner for pylint in use in some Vainu repositories",
+    version="1.1.1",
     packages=find_packages(),
-    entry_points = {
-        'console_scripts': ['vainupylinter=vainupylinter.custom_runner:run'],
-    },
+    entry_points={"console_scripts": ["vainupylinter=vainupylinter.custom_runner:run"],},
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
-    license='BSD',
+    license="BSD",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 2.7'
-    ]
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 2.7",
+    ],
 )
